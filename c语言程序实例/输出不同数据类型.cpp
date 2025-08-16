2568332452eb21b5c38954fbd5449443afa106ad
xinyu-ncut <Zhangxinyu_NCUT@outlook.com>
2025-08-16 17:00:53 +0800

    vault backup: 2025-08-16 17:00:53

--- c语言程序实例/输出不同数据类型.cpp
+++ c语言程序实例/输出不同数据类型.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
+int main()
 {
 	int n = 100;
 	char c = '@';
 	float money = 93.96;
-	printf("n=%d, c=%c,money=%f\n",n,c,money);
+	printf("n=%d, c=%c, money=%f\n", n, c, money);
 	
 	return 0;
 }
